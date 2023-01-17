59ddc44bc94887aa0306ab5d34820a2764f40321
blackseif <108239975+8lackseif@users.noreply.github.com>
2023-01-17 19:34:49 +0100

    a

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,12 @@
 var w=0;
 
+//cada vez que carge la pagina guardar en una variable w el width
+
 window.onload=function(){
 	w = window.innerWidth;
 }
+
+//cada vez que cambio el width de la pantalla compruebo si pasa del limite, si es asi realizo un reload de la pagina
 window.onresize = function(){ 
 	if(window.innerWidth > 1080 && w <= 1080){
 		location.reload();
@@ -12,7 +16,8 @@ window.onresize = function(){
 	}
 }
 	
-	
+//cargo las caracteristicas de javascript solo si el width es menor que 1080px
+
 if(window.innerWidth < 1080){
 	var opacidad2 = 0.25;
 	function opacidad(){
